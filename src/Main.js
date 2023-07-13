1c38d76c55d3e15df15016d63a5118698c5ba915
Sumit Saurav <117895961+sumitssaurabh@users.noreply.github.com>
2023-07-13 22:35:28 +0530

    Add files via upload

---
+++ src/Main.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const Main = () => {
+  return (
+   
+    
+    <div className="main">
+    <div className="div1"> 
+    
+  <div className="fbig ">YOUR FEET DESERVE THE BEST</div>
+
+
+  <div className="fsmall">
+   YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+   SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+   SHOES.
+ </div>
+
+ <div className="twobtn">
+   <button className="btn1">ShopNow</button>
+   <button className="btn2"> Category </button>
+ </div>
+
+
+ 
+ <div className="logo">
+   <h6>Also Available On </h6>
+   <img src="./pic/flipkart.png" alt="logo" />
+   <img src="./pic/amazon.png" alt="logo" />
+   </div>
+
+    </div>
+
+    
+
+
+    <div className="div2">
+    <img className="shoe" src="./pic/hero-image.png" alt="" />
+   </div>
+
+   </div>
+
+
+);
+};
+
+export default Main;
---
+++ src/Nav.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    
+      <div className="navbar">
+        <img className='img1' src="./pic/brand_logo.png" alt="logo" />
+        <ul>
+            <li href="#"> MENU </li>
+            <li href="#">  LOCATION</li>
+            <li href="#"> ABOUT </li>
+            <li href="#"> CONTACT</li>
+        </ul>
+
+
+        <button className='btn'> Login </button>
+
+      </div>
+
+     
+
+  
+  )
+}
+
+export default Nav
